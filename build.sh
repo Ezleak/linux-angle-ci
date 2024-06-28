1a51c5687cdab52059b4861c4eb0d038f0c5b399
Ezleak <116900642+Ezleak@users.noreply.github.com>
2024-06-28 16:32:04 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ pushd angle
 python scripts/bootstrap.py
 gclient sync
 ./build/install-build-deps.sh
-gn gen out/Android-arm64 --args='target_os="android" angle_enable_hlsl=true angle_enable_gl=true target_cpu="arm64" is_debug=false angle_enable_glsl=true angle_enable_swiftshader=false angle_enable_gl_desktop_frontend=true angle_enable_vulkan=true'
-autoninja -C out/Android-arm64
+gn gen out/Release --args='target_os="android" angle_enable_hlsl=true angle_enable_gl=true target_cpu="arm64" is_debug=false angle_enable_glsl=true angle_enable_swiftshader=false angle_enable_gl_desktop_frontend=true angle_enable_vulkan=true'
+autoninja -C out/Release libEGL libGLESv2
 popd
+mkdir ./artifacts
+cp angle/out/Release/*.so ./artifacts/ > /dev/null || :
